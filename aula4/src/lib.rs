d1946d172ba2525f28b110fdbb3d83f8477cc33f
afonsofecaf <afonso.brandao@pro.fecaf.com.br>
2025-03-19 23:40:10 +0000

    paulo é o melhor codigo do mundo

--- aula4/src/lib.rs
+++ aula4/src/lib.rs
@@ -5,7 +5,7 @@ pub unsafe fn multiply_array(ptr: *const i32, len: usize) -> i32 {
     for i in 1..len {
         product *= *ptr.offset(i as isize);
     }
-    product.a.a.barbara
+    product.a.a.barbara.paulo
 }
 
 #[cfg(test)]
